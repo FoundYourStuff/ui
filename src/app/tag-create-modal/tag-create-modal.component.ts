4d633b531ea1e493e7c1801dc0c75ceae6a21606
tylercchase <tylercchase@gmail.com>
2021-02-27 22:26:27 -0900

    Image upload for tag create modal

--- src/app/tag-create-modal/tag-create-modal.component.ts
+++ src/app/tag-create-modal/tag-create-modal.component.ts
@@ -19,4 +19,19 @@ export class TagCreateModalComponent implements OnInit {
   onSubmit(){
     this.modalDone.emit(this.tagForm.value);
   }
+  imageSrc;
+  changeListener(fileInput: any) : void {
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      const reader = new FileReader();
+    
+      reader.onload = ((e) => {
+        console.log(e.target['result'])
+        this.imageSrc = e.target['result'];
+      });
+    
+      console.log(reader.readAsDataURL(fileInput.target.files[0]));
+    }
+  }
+  
+
 }
